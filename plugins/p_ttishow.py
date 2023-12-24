f5f9a2f3483f12b78883045a57988682d0409b44
MasterJiraya005 <154448925+MasterJiraya005@users.noreply.github.com>
2023-12-24 17:52:54 +0300

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ', url=GRP_LNK),
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                  ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/KUSHALHK")
+                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Owner_0143")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
