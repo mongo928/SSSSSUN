49e5f1eae808623428f601a670eeed36009bf9ec
mongo928 <132264693+mongo928@users.noreply.github.com>
2024-02-02 00:42:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MasterJiraya005/SSSSSUN /SSSSSUN
+  git clone https://github.com/mongo928/SSSSSUN /SSSSSUN
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SSSSSUN
